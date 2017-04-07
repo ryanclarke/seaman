6b1803ddb8a5b792bbe4f8c00349eedfba492b05
Ryan Clarke <ryan@ryanclarke.net>
2017-04-06 20:35:12 -0400

    Add bootstrap and create basic digging and backpack

--- js/app.js
+++ js/app.js
@@ -1,36 +1,103 @@
 /*global: MainLoop*/
-;(function() {
+; (function () {
     'use strict';
 
     var events = [];
-    var logs = [];
     var state = {
+        log: {
+            messages: [],
+            draw: function () {
+                var items = '';
+                this.messages.forEach(function (msg) {
+                    items += '<li class="text-' + msg.status + '">' + msg.text + '</li>';
+                }, this);
+                id('log').innerHTML = items;
+            }
+        },
+        backpack: {
+            things: {},
+            store: function(label) {
+                if (!this.things[label]){
+                    this.things[label] = 0;
+                }
+                this.things[label] += 1;
+            },
+            draw: function () {
+                var items = '';
+                Object.keys(this.things).sort().forEach(function (thing) {
+                    items += '<li>' + thing + ': ' + this.things[thing] + '</li>';
+                }, this);
+                id('backpack').innerHTML = items;
+            }
+        },
         dig: {
             left: 0,
             duration: 2000,
-            activate: function() {
+            justFinished: false,
+            activate: function () {
                 this.left = this.duration;
             },
-            isInProgress: function() {
+            isInProgress: function () {
                 return this.left > 0;
             },
-            update: function(delta) {
-                if (this.isInProgress()){
+            update: function (delta) {
+                if (this.isInProgress()) {
                     this.left -= delta;
                     if (this.left < 0) {
                         this.left = 0;
+                        this.justFinished = true;
                     }
                 }
             },
-            draw: function() {
+            draw: function () {
                 id('dig').disabled = this.isInProgress();
-            },
+                if (this.isInProgress()) {
+                    id('dig-label').textContent = 'Digging';
+                    id('dig-bar').style.width = (100 * this.left / this.duration) + '%';
+                    id('dig-bar').classList.add('progress-bar-striped');
+                    id('dig-bar').textContent = '';
+                    id('dig-time').textContent = ' ' + num((1 + this.left) / 1000, 0) + 's';
+                }
+                if (this.justFinished) {
+                    this.justFinished = false;
+                    id('dig-label').textContent = 'Dig';
+                    id('dig-bar').style.width = '100%';
+                    id('dig-bar').classList.remove('progress-bar-striped');
+                    id('dig-bar').textContent = 'Dig';
+                    var text = '';
+                    var status = '';
+                    if (Math.floor(Math.random() * 0) == 0) {
+                        var messages = [
+                            'rock',
+                            'log',
+                            'twig',
+                            'old hat',
+                            'brick',
+                            ''
+                        ];
+                        var index = Math.floor(Math.random() * (messages.length - 1));
+                        state.backpack.store(messages[index]);
+                        text = '<strong>Dug up a ' + messages[index] + '</strong>';
+                        status = 'success';
+                    }
+                    if (text === '') {
+                        text = 'Dug in the dirt';
+                        status = 'muted';
+                    }
+                    state.log.messages.push({
+                        text: text,
+                        status: status
+                    });
+                }
+            }
         },
-        update: function(delta) {
+        update: function (delta) {
             this.dig.update(delta);
         },
-        draw: function() {
+        draw: function () {
             this.dig.draw();
+            this.backpack.draw();
+            this.log.draw();
         },
     }
 
@@ -39,7 +106,7 @@
     }
 
     function num(d, digits) {
-        return Intl.NumberFormat('en-US', {minimumFractionDigits: digits, maximumFractionDigits: digits}).format(d);
+        return Intl.NumberFormat('en-US', { minimumFractionDigits: digits, maximumFractionDigits: digits }).format(d);
     }
 
     function begin() {
@@ -48,11 +115,10 @@
             switch (event) {
                 case 'dig':
                     if (!state.dig.isInProgress()) {
-                        logs.push("Dug in the dirt");
                         state.dig.activate()
                     }
                     break;
-            
+
                 default:
                     break;
             }
@@ -64,8 +130,6 @@
     }
 
     function draw() {
-        id('log').innerHTML = logs.join("<br>");
-        id('number').textContent = num(state.dig.left/1000, 1);
         state.draw();
     }
 
@@ -84,8 +148,9 @@
 
     MainLoop.setBegin(begin).setUpdate(update).setDraw(draw).setEnd(end).start();
 
-    id('dig').addEventListener('click', function() {
+    id('dig').addEventListener('click', function () {
         events.push('dig');
     });
+    id('dig-bar').style.transition = 'width 0s ease 0s';
 })();
 
